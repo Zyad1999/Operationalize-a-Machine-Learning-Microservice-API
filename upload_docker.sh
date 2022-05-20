95e6d4f0b4c993295d3a2dc8a935a3e6e79c783f
Zyad1999 <zyadyassr17@gmail.com>
2022-05-20 22:58:08 +0000

    Create Docker and kubernetes configurations

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="zyad99/bostonhouse"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+docker tag bostonhouse zyad99/bostonhouse:latest
+    
 # Step 3:
 # Push image to a docker repository
+docker push zyad99/bostonhouse
